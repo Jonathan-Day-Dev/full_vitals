8ed366d3f494cd94b4e5383db5b0af3ba827284e
Jonathan-Day-Dev <jonday@jonday.dev>
2021-10-25 08:53:46 -0500

    initial init

---
+++ resources/js/app.js
@@ -0,0 +1,17 @@
+require('./bootstrap');
+
+import * as Vue from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import App from './vue/App.vue';
+
+const app = Vue.createApp(App);
+
+app.mount('#app');
+
+createInertiaApp({
+    resolve: name => require(`./vue/${name}`),
+    setup({ el, app, props }) {
+        Vue.createApp({ render: () => Vue.h(app, props) })
+            .mount(el)
+    }
+});
